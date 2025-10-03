005a974fac16486df7069e20f301f05c07cc1a6b
Thirupathi606 <cthirupathi2004@gmail.com>
2025-10-03 20:08:09 +0530

    session-06

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,11 +4,13 @@ USERID=$( id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "error:: please run this script with root acess"
+    exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "error:: install mysql is failure"
+    exit 1 # fialure
 else
     echo " install my sql "
